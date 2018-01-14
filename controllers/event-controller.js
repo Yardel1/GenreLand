54a8146c1642a2164e70f0dcbe58e85419a8cdf4
Yarden Hochman <yyyarden@gmail.com>
2018-01-14 14:14:34 -0500

    converted code to async await functions

--- controllers/event-controller.js
+++ controllers/event-controller.js
@@ -3,99 +3,62 @@ const Location = require('../models/location');
 const Events = require('../models/events');
 
 const eventController = {};
-/* 
-    this controller methods sends the list of zipcodes to the model
-    which brings back a list of events for each zipcode
-    it then takes the list of events and counts the number of participants
-    based on users2events
-    */
-//here we'll have a helper function that sorts the response, so we 
-            //can feed a list of events to the DB to ask for the number of participants
-            //in it.
 
-
-    /* res.json({
-                message: 'shows events for a given zipcode',
-                data: rez
-            }) */
-eventController.EventsByZips = (req, res, next) =>{
-    Events.FindByZips(res.locals.allZips)
-    .then( rez => {
+eventController.EventsByZips = async (req, res, next) => {
+    try {
+        const rez = await Events.FindByZips(res.locals.allZips)
         res.locals.events = rez
-        console.log(rez)
         next()
-        }).catch( err => console.log(err))
+    } catch (err) { console.log(err) };
 }
 
-eventController.findAll = (req, res) =>{
-    Events.findAll()
-    .then(rez => {
-        res.json({
-            message: 'show all events',
-            data: rez
-        })
-    }).catch(err =>{
-        console.log(err)
-        res.status(500).json({
-            message: 'all events page not found',
-            error: err
-        })
-    })
+eventController.findAll = async (req, res) => {
+    try {
+        const rez = await Events.findAll();
+        res.json({ message: 'show all events', data: rez });
+    }
+    catch (error) { res.status(500).json({ message: 'all events page not found', error }) };
 }
 
-eventController.findOne = (req, res) => {
-    console.log('here')
-    console.log(req.body.id)
-    Events.findById(req.body.id)
-    .then(rez =>{
-        res.json({
-            message: 'show one event',
-            data: rez
-        })
-    }).catch(err =>{
-        console.log(err)
-    })
+eventController.findOne = async (req, res) => {
+    try {
+        const rez = await Events.findById(req.body.id)
+        res.json({ message: 'show one event', data: rez })
+    }
+    catch (err) { console.log(err) };
 }
 
-eventController.create = (req, res) => {
-    Events.create(req.body.info)
-    .then(rez =>{
-        res.json({
-            message: "event created",
-            data: rez
-        }).catch(err => console.log(err))
-    }).catch(err => console.log(err))
+eventController.create = async (req, res) => {
+    try {
+        const rez = Events.create(req.body.info);
+        res.json({ message: "event created", data: rez })
+    }
+    catch (err) { console.log(err) };
 }
 
-eventController.kill = (req, res) => {
-    //the req.body.id has to be sent back 
-    //from the delete button
-    Events.destroy(req.body.id)
-    .then(() =>{
+eventController.kill = async (req, res) => {
+    try {
+        await Events.destroy(req.body.id);
         res.end();
-    }).catch(err =>{
-        console.log(err)
-    })
+    }
+    catch (err) { console.log(err) };
 }
 
-eventController.update = (req, res) =>{
-    Events.update({
-        title: req.body.title,
-        address: req.body.address,
-        event_date: req.body.date,
-        event_time: req.body.time,
-        genre: req.body.genre,
-        description: req.body.description,
-        createdby: req.body.user,
-        zip_code: req.body.zip_code  
-    }).then(rez =>{
-        res.json({
-            message: 'edit event',
-            data: rez
-        })
-    }).catch(err =>{
-        console.log(err)
-    })
+eventController.update = async (req, res) => {
+    try {
+        const rez = await Events.update({
+            title: req.body.title,
+            address: req.body.address,
+            event_date: req.body.date,
+            event_time: req.body.time,
+            genre: req.body.genre,
+            description: req.body.description,
+            createdby: req.body.user,
+            zip_code: req.body.zip_code
+        });
+        res.json({ message: 'edit event', data: rez });
+    }
+    catch (err) { console.log(err) };
 }
 
 module.exports = eventController;
--- services/zipcode.js
+++ services/zipcode.js
@@ -7,34 +7,22 @@ const axios = require('axios');
 //add above key var to url
 //.then should put the responded data in locals
 //then call next()
-function getZips(req, res, next){
+const getZips = async (req, res, next) => {
     //google places configuration
-    
-    axios(`https://www.zipcodeapi.com/rest/${process.env.zipcodeapi_API_KEY}/radius.json/${req.body.zipcode}/3/mi`)
-   .then( allZip => {
+    try {
+        const allZip = await axios(`https://www.zipcodeapi.com/rest/${process.env.zipcodeapi_API_KEY}/radius.json/${req.body.zipcode}/3/mi`)
         allZips = [
-            {
-                zip: allZip.data.zip_codes[0].zip_code
-            },
-            {
-                zip: allZip.data.zip_codes[1].zip_code   
-            },
-            {
-                zip: allZip.data.zip_codes[2].zip_code
-            },
-            {
-                zip: allZip.data.zip_codes[3].zip_code
-            },
-            {
-                zip: allZip.data.zip_codes[4].zip_code
-            }
-         ]
-        
-
-    console.log('====>', allZips)
-       res.locals.allZips = allZips
-      next()
-    }).catch( err => console.log(err) )
- }
+            {zip: allZip.data.zip_codes[0].zip_code},
+            {zip: allZip.data.zip_codes[1].zip_code},
+            {zip: allZip.data.zip_codes[2].zip_code},
+            {zip: allZip.data.zip_codes[3].zip_code},
+            {zip: allZip.data.zip_codes[4].zip_code}
+        ]
+        console.log('====>', allZips)
+        res.locals.allZips = allZips
+        next()
+    }
+    catch (err) {console.log(err)}; 
+}
 
 module.exports = {getZips};
